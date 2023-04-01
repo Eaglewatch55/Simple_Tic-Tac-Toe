b23829473716b51aad041796626bb6ae7e8b9738
Eaglewatch55 <orlandogarcia94@hotmail.com>
2023-04-01 12:25:48 -0600

    - Stage 4/5

--- Simple Tic-Tac-Toe (Python)/task/tictactoe.py
+++ Simple Tic-Tac-Toe (Python)/task/tictactoe.py
@@ -1,34 +1,70 @@
-def matrix_generator(string):
+def matrix_generator(input_string):
     dict_ = {}
 
     mat = [[input_string[0], input_string[1], input_string[2]],
               [input_string[3], input_string[4], input_string[5]],
               [input_string[6], input_string[7], input_string[8]]]
 
-    for char in input_string:
-        if char not in accepted_char:
-            raise ValueError(f"Character '{char}' not accepted")
+    for char_ in input_string:
+        if char_ not in accepted_char:
+            raise ValueError(f"Character '{char_}' not accepted")
 
-        if char not in dict_.keys():
-            dict_[char] = 1
+        if char_ not in dict_.keys():
+            dict_[char_] = 1
 
         else:
-            dict_[char] += 1
+            dict_[char_] += 1
 
     return mat, dict_
 
-#XOOOXOXXO
-input_string = input()
+
+def print_matrix(temp_matrix):
+    print("---------")
+    print(f"| {temp_matrix[0][0]} {temp_matrix[0][1]} {temp_matrix[0][2]} |")
+    print(f"| {temp_matrix[1][0]} {temp_matrix[1][1]} {temp_matrix[1][2]} |")
+    print(f"| {temp_matrix[2][0]} {temp_matrix[2][1]} {temp_matrix[2][2]} |")
+    print("---------")
+
+
+def update_matrix(temp_matrix, cord):
+    temp_matrix[cord[0]][cord[1]] = "X"
+    return temp_matrix
+
+input_ = input()
 accepted_char = ("X", "O", "_")
 
-matrix, dict_count = matrix_generator(input_string)
+matrix, dict_count = matrix_generator(input_)
 
-print("---------")
-print(f"| {matrix[0][0]} {matrix[0][1]} {matrix[0][2]} |")
-print(f"| {matrix[1][0]} {matrix[1][1]} {matrix[1][2]} |")
-print(f"| {matrix[2][0]} {matrix[2][1]} {matrix[2][2]} |")
-print("---------")
+print_matrix(matrix)
 
+while True:
+    input_ = input()
+    coordinates = []
+
+    try:
+        for char_ in input_:
+            if char_ == " ":
+                continue
+            else:
+                coordinates.append(int(char_) - 1)
+    except ValueError:
+        print("You should enter numbers!")
+        continue
+
+    valid_num = [0, 1, 2]
+
+    if coordinates[0] not in valid_num or coordinates[1] not in valid_num:
+        print("Coordinates should be from 1 to 3!")
+        continue
+
+    if matrix[coordinates[0]][coordinates[1]] != "_":
+        print("This cell is occupied! Choose another one!")
+        continue
+
+    break
+
+matrix = update_matrix(matrix, coordinates)
+print_matrix(matrix)
 
 # VALIDATION AND STATUS CHECKER
 
